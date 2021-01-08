a03d08504b3ee2b7b86ff8abc7ea2269116710c5
wujekstaszek <jaolekm@gmail.com>
2021-01-08 01:56:08 +0100

    wtf

--- pdwco/settings.py
+++ pdwco/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ALLOWED_HOSTS = ["chrobry.herokuapp.com"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
