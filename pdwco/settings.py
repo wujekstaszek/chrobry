0dfd82d54cbe97c60c8bb901be1eec4dc7d06af5
Alekasnder Morgała <jaolekm@gmail.com>
2021-01-18 13:07:39 +0100

    Update settings.py

--- pdwco/settings.py
+++ pdwco/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm-tvrliz0#t+)lo7is^c)m7rdsy%@r@73#gb1%7(91*441g6!r'
+SECRET_KEY = 'NOPE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
